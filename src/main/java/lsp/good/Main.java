910554fda14ae5ab35aba4c24bc99853a89377aa
DavidPolSanchez <davidpolsanchezm98@gmail.com>
2022-09-20 15:44:43 +0200

    create good example for Liskov substitution

---
+++ src/main/java/lsp/good/Main.java
@@ -0,0 +1,18 @@
+package lsp.bad;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        // este codigo seria el ideal a ejecutar
+        Vehicle combusion = new CombustionCar();
+        Vehicle electric = new ElectricCar();
+        combusion.accelerate();
+        electric.accelerate();
+        
+
+    }
+    
+}
